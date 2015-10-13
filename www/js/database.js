d24982066837577046483b2c755977a4c5f72d0e
RoenMidnight <brunocesara@hotmail.com>
2015-10-13 13:33:45 -0300

    Alterando

---
+++ www/js/database.js
@@ -0,0 +1,12 @@
+var db;
+
+function openDataBase() {
+	db = window.sqlitePlugin.openDatabase({ name: "TCalc.db", createFromLocation: 1 });
+		
+	db.transaction(function(tx){
+		tx.executeSql("select count(id) as Tal from Talento;", [], function(tx, res){
+			console.log("res.rows.length: "+ res.rows.length + " -- deveria ser 1");
+			console.log("res.rows.item(0).Tal: "+ res.rows.item(0).Tal + " -- deveria ser 1");
+		});	
+	});
+}
